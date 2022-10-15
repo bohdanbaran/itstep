1e4c14238a59ebd548ba90cd6657d736c6044e16
bohdanbaran <bohdan.baran@gmail.com>
2022-10-15 15:37:49 +0300

    initial commit

---
+++ 2(3).py
@@ -0,0 +1,9 @@
+class Student:
+ def __init__(self, name=None):
+    self.name = name
+
+ def __str__(self):
+        return f"I am a student. My name is {self.name}."
+
+nick = Student(name="Nick")
+print(nick)
