1bd741f2ba5e34f5eb6e246742cd7874715f7e6d
bohdanbaran <bohdan.baran@gmail.com>
2022-10-21 23:11:06 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 class Student:
-    print("Hi")
     def __init__(self):
-        self.height = 160
-        print("I am alive!")
-first_student = Student()
+         self.height = 170
+    height = 160
+    def printer(self):
+        print(self.height)
+nick = Student()
+nick.printer()
